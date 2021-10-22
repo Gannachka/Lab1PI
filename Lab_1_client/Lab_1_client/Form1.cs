cc4618d857ae4fcaac1ec2c4d0e0e16fdc2a078f
Hanna Miklyaeva <herring01001@gmail.com>
2021-10-22 09:08:18 +0300

    Add Labs

---
+++ Lab_1_client/Lab_1_client/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Flurl.Http;
+
+namespace Lab_1_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+            var responseString = await "https://localhost:44351/sum"
+    .PostUrlEncodedAsync(new { x = textBox1.Text, y = textBox2.Text })
+    .ReceiveString();
+            
+            textBox3.Text = responseString;
+        }
+    }
+}
